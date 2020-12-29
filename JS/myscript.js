8a845c8a1a1be4508c828371a260722b576b96bd
Wanuja Ranasinghe <63337783+Wanuja97@users.noreply.github.com>
2020-12-29 17:19:05 +0530

    Add files via upload

---
+++ JS/myscript.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("login-form");
+var y = document.getElementById("register-form");
+var z = document.getElementById("register-btn");
+
+function register(){
+x.style.left = "-400px";
+y.style.left = "50px";
+z.style.left = "110px";
+}
+
+function login(){
+x.style.left = "50px";
+y.style.left = "450px";
+z.style.left = "0px";
+}
